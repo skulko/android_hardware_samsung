5221b97551ca598a4cb0c3dba2485432368062f4
tobigun <hennymcc@yahoo.de>
2016-05-27 13:29:42 +0200

    ril: add support for ss222 (G800F/Y/M)
    Change-Id: Id1dff668f979ede208a0defa83ab6f49b4b3e712

--- ril/libril/ril.cpp
+++ ril/libril/ril.cpp
@@ -117,6 +117,8 @@ static pthread_t s_tid_dispatch;
 static pthread_t s_tid_reader;
 static int s_started = 0;
 
+static int s_cpCrashed = 0;
+
 static int s_fdDebug = -1;
 static int s_fdDebug_socket2 = -1;
 
@@ -912,6 +914,14 @@ void RIL_onUnsolicitedResponse(int unsolResponse, const void *data,
         memcpy(s_lastNITZTimeData, data, datalen);
     }
 
+    if (unsolResponse == RIL_UNSOL_AM) {
+        // "start -a android.intent.action.MAIN -n com.sec.app.RilErrorNotifier/.PhoneCrashNotifier --es title cpcrash"
+        if (strstr((const char*)data, "cpcrash")) {
+            RLOGE("CP crash detected");
+            s_cpCrashed = 1;
+        }
+    }
+
     // Normal exit
     return;
 
@@ -919,6 +929,14 @@ error_exit:
     if (shouldScheduleTimeout) {
         releaseWakeLock();
     }
+}
+
+extern "C"
+int RIL_getCpCrashed()
+{
+    int result = s_cpCrashed;
+    s_cpCrashed = 0;
+    return result;
 }
 
 /** FIXME generalize this if you track UserCAllbackInfo, clear it
